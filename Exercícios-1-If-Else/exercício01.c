8dd838d374f7a62674c55ee4bf21ad10f87575b3
mmaximus-bit <maximusmiguel68@gmail.com>
2025-07-10 09:56:51 -0300

    Fiz o segundo exercicio if e else

--- Exercícios-1-If-Else/exercício01.c
+++ Exercícios-1-If-Else/exercício01.c
@@ -1,2 +1,31 @@
+/*
+    Dado um número, determinar se ele é positivo, negativo ou zero.
+*/
+
+
 #include <stdio.h>
 
+int main(){
+
+    float num;
+
+    printf("Ola digite um numero qualquer :");
+    scanf("%f",&num);
+
+    if(num < 0){
+
+        printf("Seu numero eh negativo.");
+    }
+    
+    else if (num > 0)
+    {
+        printf("Seu numero eh positivo.");
+    } 
+    
+    else {
+
+        printf("Seu numero eh igual a zero.");
+    }
+    
+    return 0;
+}
---
+++ Exercícios-1-If-Else/exercício02.c
@@ -0,0 +1,25 @@
+/*
+    Dado um ano qualquer, veriicar se ele é bissexto.
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int ano;
+
+    printf("Digite um ano qualquer :");
+    scanf("%d", &ano);
+
+    if((ano % 4 == 0) && (ano % 100 != 0)|| (ano % 400 == 0) ){
+
+        printf("O ano %d eh um ano bissexto", ano);
+    } else{
+
+        printf("O ano %d nao eh um ano bissexto", ano);
+
+    }
+
+    return 0;
+}
+//Um ano bissexto é divisível por 4 e só é divisível por 100 se ao mesmo tempo for divisível por 400
